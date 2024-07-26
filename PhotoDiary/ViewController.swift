217e891fa7ffd2db28da8efc9dab6d84bdb2a66d
Huang, Yi-Tang <ty80517@gmail.com>
2024-07-26 14:05:35 +0800

    Initial Commit

---
+++ PhotoDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoDiary
+//
+//  Created by 黃翊唐 on 2024/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
